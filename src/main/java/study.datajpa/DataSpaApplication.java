403bf4d6abc69dfb68e9cabad904f72cdfa026a9
조영혜 <0808yeonghye@gmail.com>
2022-07-04 22:45:27 +0900

    라이브러리 설정

---
+++ src/main/java/study.datajpa/DataSpaApplication.java
@@ -0,0 +1,13 @@
+package study.dataspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSpaApplication.class, args);
+	}
+
+}
